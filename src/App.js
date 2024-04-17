3fb81b8f1908ac4e8d5ff1abfa550eb13cac108a
Ahmed Mahmoud <ahmedmahmoudtech@gmail.com>
2024-04-18 01:21:11 +0200

    0

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   
   if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/tower/sw.js')
       .then(registration => {
         console.log('Service Worker registered:', registration);
       })
--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 
 function Loading() {
   return (
-
     <div className="loadingContainer">
-        <div className="a"><span>L</span></div>
-        <div className="b"><span>O</span></div>
-        <div className="c"><span>A</span></div>
-        <div className="d"><span>D</span></div>
-        <div className="e"><span>I</span></div>
-        <div className="f"><span>N</span></div>
-        <div className="g"><span>G</span></div>
-    </div>
+      <div className="barContainer">
+
+  <div class="bar">
+  <div class="circle"></div>
+  <p>Scene Loading</p>
+</div>
+      </div>
+</div>
  
    )
 }
